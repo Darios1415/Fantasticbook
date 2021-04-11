0b1e6c81cde1626ce072d5c950199abdabef88dd
Darios1415 <65882801+Darios1415@users.noreply.github.com>
2021-04-11 17:31:17 -0500

    validaciones

--- resources/views/Cruds/autor/create.blade.php
+++ resources/views/Cruds/autor/create.blade.php
@@ -18,87 +18,105 @@
             <div class="card-header">
                 <h3 class="card-title">Autor</h3>
             </div>
-                <form action="{{route('autor.store')}}" method="post" enctype="multipart/form-data">
+                <form action="{{route('autor.store')}}" method="post" enctype="multipart/form-data" class="row g-3 needs-validation" novalidate>
                 {{csrf_field()}}
                 <div class="card-body">
                         <div class="form-group">
                         <div class="row">
                             <div class="col">
-                            <label>Nombre:</label>
-                            <input type="text" name="nombre" placeholder="Nombre" value="{{old('nombre')}}" class="form-control">
-                            {!! $errors->first('nombre', '<small class="text-danger"">:message</small>') !!}
+                            <label  for="validationnombre">Nombre:</label>
+                            <input type="text" name="nombre" placeholder="Nombre" value="{{old('nombre')}}" class="form-control" id="validationnombre" pattern="[A-Za-Z]" required>
+                            <div class="invalid-feedback">
+                                Nombre requerido.
+                            </div>
                             </div>
                             <div class="col">
-                                <label>Apellido Paterno:</label>
-                            <input type="text" name="app" placeholder="Ap.paterno" value="{{old('app')}}" class="form-control">
-                            {!! $errors->first('app', '<small class="text-danger"">:message</small>') !!}
+                                <label for="validationapp">Apellido Paterno:</label>
+                            <input type="text" name="app" placeholder="Ap.paterno" value="{{old('app')}}" class="form-control" id="validationapp" pattern="[A-Za-Z]" required>
+                            <div class="invalid-feedback">
+                                Apellido Paterno requerido.
+                            </div>
                             </div>
                             <div class="col">
-                                <label>Apellido Materno:</label>
-                            <input type="text" name="apm" placeholder="Ap. Materno" value="{{old('apm')}}" class="form-control">
-                            {!! $errors->first('apm', '<small class="text-danger"">:message</small>') !!}
+                                <label for="validationapm">Apellido Materno:</label>
+                            <input type="text" name="apm" placeholder="Ap. Materno" value="{{old('apm')}}" class="form-control" id="validationapm" pattern="[A-Za-Z]" required>
+                            <div class="invalid-feedback">
+                                Apellido Materno  requerido.
+                            </div>
                             </div>
                         </div>
                         </div>
                         <div class="form-group">
                         <div class="row">
                             <div class="col">
-                                <label>Fecha de Nacimiento:</label>
-                            <input type="date" name="fecha_na" value="{{old('fecha_na')}}" class="form-control">
-                            {!! $errors->first('fecha_na', '<small class="text-danger"">:message</small>') !!}
+                                <label for="validationfecha_na">Fecha de Nacimiento:</label>
+                            <input type="date" name="fecha_na" value="{{old('fecha_na')}}" class="form-control" id="validationfecha_na" required>
+                            <div class="invalid-feedback">
+                                Fecha de nacimiento requerido.
+                            </div>
                             </div>
                             <div class="col">
-                            <label>Nacionalidad:</label>
-                            <input type="text" name="nacionalidad" placeholder="Nacionalidad" value="{{old('nacionalidad')}}" class="form-control">
-                            {!! $errors->first('nacionalidad', '<small class="text-danger"">:message</small>') !!}
+                            <label for="validationnacionalidad">Nacionalidad:</label>
+                            <input type="text" name="nacionalidad" placeholder="Nacionalidad" value="{{old('nacionalidad')}}" class="form-control" id="validationnacionalidad" pattern="[A-Za-Z]" required>
+                            <div class="invalid-feedback">
+                                Nacionalidad requerido.
                             </div>
-                            
+                            </div>
                         </div>
                         </div>
                         <div class="form-group">
                         <div class="row">
                             <div class="col">
-                                <label>Clave Interbancaria:</label>
-                            <input type="text" name="clave_inter" placeholder="clave" value="{{old('clave_inter')}}" class="form-control">
-                            {!! $errors->first('clave_inter', '<small class="text-danger"">:message</small>') !!}
+                                <label for="validationclave_inter">Clave Interbancaria:</label>
+                            <input type="text" name="clave_inter" placeholder="clave" value="{{old('clave_inter')}}" class="form-control" id="validationclave_inter" pattern="[0-9]{18}" required>
+                            <div class="invalid-feedback">
+                                Clave interbancario debe contener 18 digitos.
+                            </div>
                             </div>
                             <div class="col">
-                                <label for="validationServer03" class="form-label">Género</label>
+                                <label{
+                                    
+                                } class="form-label">Género</label>
                                 <div class="custom-control custom-radio">
-                                <input class="custom-control-input" type="radio" value="Masculino" name="sexo" checked="">
-                                <label for="customRadio1" class="custom-control-label">Masculino</label>
+                                <input class="custom-control-input" type="radio" value="Masculino" name="sexo" id="validationgenero1" required>
+                                <label for="validationgenero1" class="custom-control-label">Masculino</label>
                                 </div>
                                 <div class="custom-control custom-radio">
-                                <input class="custom-control-input" type="radio" value="Femenino" name="sexo" >
-                                <label for="customRadio2" class="custom-control-label" >Femenino</label>
+                                <input class="custom-control-input" type="radio" value="Femenino" name="sexo" id="validationgenero2"  required >
+                                <label for="validationgenero2" class="custom-control-label" >Femenino</label>
                                 </div>
-                                {!! $errors->first('sexo', '<small class="text-danger"">:message</small>') !!}
                             </div>
                             </div>
                             </div>
                             <div class="form-group">
                             <div class="row">
                             <div class="col">
-                                <label>Foto del Autor: </label>
-                                <input type="file" name="foto" placeholder="Foto">
-                                {!! $errors->first('foto', '<br><small class="text-danger"">:message</small>') !!}
+                                <label for="validationfoto">Foto del Autor: </label>
+                                <input type="file" name="foto" placeholder="Foto" accept=".png .jpg .jpeg" required>
+                                <div class="invalid-feedback">
+                                    Foto requerida.
+                                </div>
                             </div>
                             <div class="col">
-                                <label>Genero:</label>
-                                <select name="idgen" class="form-control">
-                                <option>--Seleccione un Genero--</option>
+                                <label for="validationgenero">Genero:</label>
+                                <select name="idgen" class="form-control" id="validationgenero" required>
+                                <option value="">--Seleccione un Genero--</option>
                                 @foreach($genero as $gen)
                                 <option value="{{$gen->idgen}}">{{$gen ->nombre}}</option>
                                 @endforeach
                             </select>
-                            {!! $errors->first('idgen', '<small class="text-danger"">:message</small>') !!}
+                            <div class="invalid-feedback">
+                                Genero requerido.
+                            </div>
                                 </div>
                             </div>
                             </div>
                             <div class="form-group">
-                            <label>Biografia:</label><br>
-                                <textarea name="biografia" class="form-control">{{old('biografia')}}</textarea>
-                                {!! $errors->first('biografia', '<small class="text-danger"">:message</small>') !!}
+                            <label for="validationbiografia">Biografia:</label><br>
+                                <textarea name="biografia" class="form-control" id="validationbiografia" pattern="[A-Za-z0-9]{18, 18}" required>{{old('biografia')}}</textarea>
+                                <div class="invalid-feedback">
+                                    Biografia requerido.
+                                </div>
                             </div>
                         <div class="card-footer">
                         <input type="submit" value="Enviar" class="btn btn-primary">
@@ -117,4 +135,26 @@
 
 @section('js')
     <script> console.log('Hi!'); </script>
-@stop
+<script>
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+</script>
+    @stop
--- resources/views/Cruds/autor/edit.blade.php
+++ resources/views/Cruds/autor/edit.blade.php
@@ -25,56 +25,68 @@
                     <div class="form-group">
                     <div class="row">
                         <div class="col">
-                            <label>Nombre:</label>
-                            <input type="text" name="nombre" placeholder="Nombre" value="{{$autor->nombre}}" class="form-control">
-                            {!! $errors->first('nombre', '<small class="text-danger"">:message</small>') !!}
+                            <label   for="validationnombre">Nombre:</label>
+                            <input type="text" name="nombre" placeholder="Nombre" value="{{$autor->nombre}}" class="form-control" id="validationnombre" pattern="[A-Za-Z]" required>
+                            <div class="invalid-feedback">
+                                Nombre requerido.
+                            </div>
                         </div>
                         <div class="col">
-                            <label>Apellido Paterno:</label>
-                            <input type="text" name="app" placeholder="Ap.paterno" value="{{$autor->app}}" class="form-control">
-                            {!! $errors->first('app', '<small class="text-danger"">:message</small>') !!}
+                            <label   for="validationapp">Apellido Paterno:</label>
+                            <input type="text" name="app" placeholder="Ap.paterno" value="{{$autor->app}}" class="form-control" id="validationapp" pattern="[A-Za-Z]" required>
+                            <div class="invalid-feedback">
+                                Apellido Paterno requerido.
+                            </div>
                         </div>
                         <div class="col">
-                            <label>Apellido Materno:</label>
-                            <input type="text" name="apm" placeholder="Ap. Materno" value="{{$autor->apm}}" class="form-control">
-                            {!! $errors->first('apm', '<small class="text-danger"">:message</small>') !!}
+                            <label  for="validationapm">Apellido Materno:</label>
+                            <input type="text" name="apm" placeholder="Ap. Materno" value="{{$autor->apm}}" class="form-control" id="validationapm" pattern="[A-Za-Z]" required>
+                            <div class="invalid-feedback">
+                                Apellido Materno  requerido.
+                            </div>
                         </div>
                     </div>
                     </div>
                     <div class="form-group">
                     <div class="row">
                         <div class="col">
-                            <label>Fecha de Nacimiento:</label>
-                            <input type="date" name="fecha_na" value="{{$autor->fecha_na}}" class="form-control">
-                            {!! $errors->first('fecha_na', '<small class="text-danger"">:message</small>') !!}
+                            <label   for="validationfecha_na">Fecha de Nacimiento:</label>
+                            <input type="date" name="fecha_na" value="{{$autor->fecha_na}}" class="form-control" id="validationfecha_na" required>
+                            <div class="invalid-feedback">
+                                Fecha de nacimiento requerido.
+                            </div>
                         </div>
                         <div class="col">
-                            <label>Nacionalidad:</label>
-                            <input type="text" name="nacionalidad" placeholder="Nacionalidad" value="{{$autor->nacionalidad}}" class="form-control">
-                            {!! $errors->first('nacionalidad', '<small class="text-danger"">:message</small>') !!}
+                            <label   for="validationnacionalidad">Nacionalidad:</label>
+                            <input type="text" name="nacionalidad" placeholder="Nacionalidad" value="{{$autor->nacionalidad}}" class="form-control" id="validationnacionalidad" pattern="[A-Za-Z]" required>
+                            <div class="invalid-feedback">
+                                Nacionalidad requerido.
+                            </div>
                         </div>    
                     </div>
                     </div>
                     <div class="form-group">
                     <div class="row">
                         <div class="col">
-                            <label>Clave Interbancaria:</label>
-                            <input type="text" name="clave_inter" placeholder="clave" value="{{$autor->clave_inter}}" class="form-control">
-                            {!! $errors->first('clave_inter', '<small class="text-danger"">:message</small>') !!}
+                            <label   for="validationclave_inter">Clave Interbancaria:</label>
+                            <input type="text" name="clave_inter" placeholder="clave" value="{{$autor->clave_inter}}" class="form-control" id="validationclave_inter" pattern="[0-9]{18}" required>
+                            <div class="invalid-feedback">
+                                Clave interbancario debe contener 18 digitos.
+                            </div>
                         </div>
                         
                         <div class="col-sm-6">
-                            <label for="validationServer03" class="form-label">Sexo</label>
+                            <label class="form-label">Sexo</label>
                             <!-- radio -->
                             <div class="form-group">
                             @if($autor->sexo == 'Masculino')
                                 <div class="custom-control custom-radio" >
                                 <input class="custom-control-input" type="radio" id="customRadio1" name="sexo" value="Masculino" checked="">
-                                <label for="customRadio1" class="custom-control-label">Masculino</label>
+                                <label   for="validationgenero1" class="custom-control-label">Masculino</label>
                             </div>
                             <div class="custom-control custom-radio">
                                 <input class="custom-control-input" type="radio" id="customRadio2" name="sexo" value="Femenino" >
-                                <label for="customRadio2" class="custom-control-label" >Femenino</label>
+                                <label   for="validationgenero2" class="custom-control-label" >Femenino</label>
                             </div>
                             @else
                             <div class="custom-control custom-radio" >
@@ -84,7 +96,7 @@
                             <div class="custom-control custom-radio">
                                 <input class="custom-control-input" type="radio" id="customRadio2" name="sexo" value="Femenino" checked="">
                                 <label for="customRadio2" class="custom-control-label" >Femenino</label>
-                            </div>
+                            </div>3
                             @endif
                             </div>
                         </div>
@@ -93,13 +105,13 @@
                             <div class="form-group">
                             <div class="row">
                             <div class="col">
-                                <label>Foto Autor: </label>
-                                <input type="file" name="foto" placeholder="Foto" value="{{$autor->foto}}" onchange="preview(this)">
+                                <label   for="validationnombre">Foto Autor: </label>
+                                <input type="file" name="foto" placeholder="Foto" value="{{$autor->foto}}" onchange="preview(this)" accept="image/png, image/jpeg">
                                 <br>
                                 <img src="/img/autor/{{$autor->foto}}" width="100px" id="foto" alt="">
                             </div>
                             <div class="col">
-                                <label>Genero Literario:</label>
+                                <label   for="validationgenero">Genero Literario:</label>
                                 <select name="idgen" class="form-control">
                                 <option>--Seleccione Genero--</option>
                                 @foreach($genero as $gen)
@@ -113,9 +125,11 @@
                             </div>
                             </div>
                             <div class="form-group">
-                            <label>Biografia:</label><br>
-                                <textarea name="biografia" class="form-control">{{$autor->biografia}}</textarea>
-                                {!! $errors->first('biografia', '<small class="text-danger"">:message</small>') !!}
+                            <label   for="validationbiografia">Biografia:</label><br>
+                                <textarea name="biografia" class="form-control" id="validationbiografia" pattern="[A-Za-z0-9]{18, 18}" required>{{$autor->biografia}}</textarea>
+                                <div class="invalid-feedback">
+                                    Biografia requerido.
+                                </div>
                             </div>
                 <div class="card-footer">
                         <input type="submit" value="Editar" class="btn btn-primary">
@@ -166,35 +180,28 @@
         }
     }
     }
-    </script><script>
-    function preview(e)
-{
-if(e.files && e.files[0]){
-    // Comprobamos que sea un formato de imagen
-    if (e.files[0].type.match('image.*')) {
-        // Inicializamos un FileReader. permite que las aplicaciones web lean
-        // ficheros (o información en buffer) almacenados en el cliente de forma
-        // asíncrona
-        // Mas info en: https://developer.mozilla.org/es/docs/Web/API/FileReader
-        var reader=new FileReader();
-        // El evento onload se ejecuta cada vez que se ha leido el archivo
-        // correctamente
-        reader.onload=function(e) {
-            document.getElementById("foto").src=e.target.result;
+    </script>
+    
+    <script>
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
         }
-        // El evento onerror se ejecuta si ha encontrado un error de lectura
-        reader.onerror=function(e) {
-            document.getElementById("foto").scr="";
-            alert('Hubo un error al cargar la imagen');
-        }
-        // indicamos que lea la imagen seleccionado por el usuario de su disco duro
-        reader.readAsDataURL(e.files[0]);
-    }else{
-        // El formato del archivo no es una imagen
-        document.getElementById("foto").src="";
-        alert("El archivo adjunto no es una imagen");
-    }
-}
-}
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
 </script>
 @stop
